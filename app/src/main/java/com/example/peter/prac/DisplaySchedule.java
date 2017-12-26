5fad38014727497f6c6f030dbf3900e07523a582
Peter Youn <ryoun@college.harvard.edu>
2017-12-26 10:04:08 +0900

    Click on a day correctly leads to TextView.
    TODO: implement sort feature in Schedule and sort before outputting a day's schedule by beginTime

--- app/src/main/java/com/example/peter/prac/DisplaySchedule.java
+++ app/src/main/java/com/example/peter/prac/DisplaySchedule.java
@@ -1,8 +1,11 @@
 package com.example.peter.prac;
 
+import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -32,5 +35,14 @@ public class DisplaySchedule extends AppCompatActivity {
                 R.layout.display_schedule_row, schedules);
         lView.setAdapter(lAdapter);
 
+        lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent = new Intent(view.getContext(), DisplayScheduleInfo.class);
+                String pos = parent.getPositionForView(view) + "";
+                myIntent.putExtra("clickPosition", pos);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/peter/prac/DisplayScheduleInfo.java
@@ -0,0 +1,67 @@
+package com.example.peter.prac;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.opencsv.CSVReader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Peter on 12/26/2017.
+ */
+
+public class DisplayScheduleInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_schedule_info);
+
+        TextView scheduleInfo = (TextView) findViewById(R.id.ScheduleInfo);
+        //scheduleInfo.setMovementMethod(new ScrollingMovementMethod());
+        Bundle extras = getIntent().getExtras();
+        String position = extras.getString("clickPosition");
+        position = "1";
+        String fileName = "schedule_day" + position + ".csv";
+
+        AssetManager assetManager = getResources().getAssets();
+        InputStream inputStream = null;
+        List<Schedule> tempSchedule = new ArrayList<Schedule>();
+
+        try {
+            inputStream = assetManager.open(fileName);
+            if (inputStream != null) {
+                Log.d("X", "STREAM SUCCESS");
+            }
+            CSVReader reader = new CSVReader(new InputStreamReader(inputStream,
+                    "UTF-8"));
+            String[] line;
+            reader.readNext();
+            while ((line = reader.readNext()) != null) {
+                tempSchedule.add(new Schedule(
+                        Integer.parseInt(line[0]),
+                        line[1], line[2],
+                        line[3], line[4], line[5]));
+            }
+
+            Schedule[] scheduleList = new Schedule[ tempSchedule.size() ];
+            tempSchedule.toArray(scheduleList);
+
+            String dispTxt = "";
+            
+            scheduleInfo.setText(dispTxt);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
